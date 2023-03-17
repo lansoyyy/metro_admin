d469b4d38402eb369b4f37797b265f3a8e6754cd
Lans12345 <olanalans12345@gmail.com>
2023-03-17 11:41:12 +0800

    added auth

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:metro_admin/screens/home_screen.dart';
 import 'package:metro_admin/utils/colors.dart';
 import 'package:metro_admin/widgets/button_widget.dart';
+import 'package:metro_admin/widgets/text_widget.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  late String username;
+  late String password;
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +27,48 @@ class LoginPage extends StatelessWidget {
               width: 200,
             ),
             const SizedBox(height: 50),
-            const SizedBox(
+            SizedBox(
               width: 300,
               child: TextField(
-                decoration: InputDecoration(
+                onChanged: (value) {
+                  username = value;
+                },
+                decoration: const InputDecoration(
                   labelText: 'Username',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
             const SizedBox(height: 20),
-            const SizedBox(
+            SizedBox(
               width: 300,
               child: TextField(
+                onChanged: (value) {
+                  password = value;
+                },
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
                 ),
               ),
             ),
             const SizedBox(height: 30),
-            ButtonWidget(color: secondaryRed, label: 'Login', onPressed: () {})
+            ButtonWidget(
+                color: secondaryRed,
+                label: 'Login',
+                onPressed: () {
+                  if (username == 'cvkatcoadmin' && password == 'cvkatco') {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: TextRegular(
+                            text: 'Invalid admin credentials',
+                            fontSize: 18,
+                            color: Colors.white)));
+                  }
+                })
           ],
         ),
       ),
